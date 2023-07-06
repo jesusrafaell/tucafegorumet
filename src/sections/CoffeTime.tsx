0a1d16cbd7b9e7d0a9bd1058252c3f10cf6d2c7d
Jesus Rafaell <37907710+jesusrafaell@users.noreply.github.com>
2023-07-06 18:13:49 -0400

    fixef CoffeTime.tsx

--- src/sections/CoffeTime.tsx
+++ src/sections/CoffeTime.tsx
@@ -97,9 +97,9 @@ export const CoffeTime = () => {
 						animate={controlInfo}
 						className='relative text-white w-[400x] h-[200px]'
 					>
-						<h1 className='text-2xl lg:text-5xl font-bold lg:flex  font-satoshi my-5' variants={fadeInUp}>
+						<motion.h1 className='text-2xl lg:text-5xl font-bold lg:flex  font-satoshi my-5' variants={fadeInUp}>
 							{selectCup.title}
-						</h1>
+						</motion.h1>
 						<p className='text-[15px] lg:text-[17px] font-light whitespace-normal text-justify max-h-full leading-[1.4] py-5 w-full lg:w-[500px]'>
 							{selectCup.info}
 						</p>
